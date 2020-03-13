f045c04343c1a0f6e5a60130dbaf90056d8d87e5
Pedro del Rio <pedelriomarron@gmail.com>
2020-03-13 04:12:44 +0100

    Updates

---
+++ precache-manifest.00c85e779fa6d07cf4e464003f856b1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11d73d1fceaf2c44a43f55b26a09ff52",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "030e4ee44a3d758ffa89",
+    "url": "/web/static/css/main.ab1adefc.chunk.css"
+  },
+  {
+    "revision": "007850677cf2bae3d0a1",
+    "url": "/web/static/js/2.6953143b.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/web/static/js/2.6953143b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "030e4ee44a3d758ffa89",
+    "url": "/web/static/js/main.ccb5d865.chunk.js"
+  },
+  {
+    "revision": "bb98d9868c556937e3ac",
+    "url": "/web/static/js/runtime-main.b1e22561.js"
+  },
+  {
+    "revision": "bccab4b11801b0f42dfada79ed7b353b",
+    "url": "/web/static/media/sign.bccab4b1.svg"
+  }
+]);
